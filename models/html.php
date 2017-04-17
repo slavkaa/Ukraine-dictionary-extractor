f1b50a0ac8762e6258f80ad2f309c95bb4a85732
slavka <nikoolin@ukr.net>
2017-04-17 23:01:15 +0300

    Handle exclamation

--- models/html.php
+++ models/html.php
@@ -183,4 +183,44 @@ class Html extends AbstractModel {
 
         return $stm;
     }
+
+    /**
+     * @param string $word
+     * @param string $partOfLanguage
+     * @param integer $dictionaryId
+     */
+    public function firstOrNewByPartOfLanguage($word, $partOfLanguage, $dictionaryId)
+    {
+        $sql = 'SELECT * FROM `' . $this->tableName . '` WHERE part_of_language = :part_of_language AND word_binary = :word';
+        $stm = $this->connection->prepare($sql);
+        $stm->bindParam(':word', $word, PDO::PARAM_STR);
+        $stm->bindParam(':part_of_language', $partOfLanguage, PDO::PARAM_STR);
+        $stm->execute();
+        $result = $stm->fetch(PDO::FETCH_ASSOC);
+
+        if (!empty($result)) {
+            echo 'U[html] ';
+        } else {
+            echo 'I[html] ';
+            $sql = 'INSERT INTO `' . $this->tableName .
+                '` (`dictionary_id`,`word`, `word_binary`, `part_of_language`) VALUES (:dictionary_id, :word, :word, :part_of_language);';
+            $stm = $this->connection->prepare($sql);
+            $stm->bindParam(':word', $word, PDO::PARAM_STR);
+            $stm->bindParam(':dictionary_id', $dictionaryId, PDO::PARAM_INT);
+            $stm->bindParam(':part_of_language', $partOfLanguage, PDO::PARAM_STR);
+            $stm->execute();
+            $id = $this->connection->lastInsertId();
+
+//            var_dump($this->connection->errorInfo());
+
+            $sql = 'SELECT * FROM `' . $this->tableName . '` WHERE id = :id limit 1;';
+            $stm = $this->connection->prepare($sql);
+            $stm->bindParam(':id', $id);
+            $stm->execute();
+            $result = $stm->fetch(PDO::FETCH_ASSOC);
+        }
+
+        $this->id = array_get($result, 'id');
+        $this->props = $result;
+    }
 }
---
+++ slovnyk_ua_handle_exclamation.php
@@ -0,0 +1,53 @@
+﻿<?php
+
+// @link: http://phpfaq.ru/pdo
+// @acton: php slovnyk_ua_handle_exclamation.php    Вигук
+
+require_once('support/config.php');
+require_once('support/functions.php');
+require_once('support/libs.php');
+require_once('models/word.php');
+require_once('models/wordToIgnore.php');
+require_once('models/source.php');
+require_once('models/wordToSource.php');
+require_once('models/dictionary.php');
+require_once('models/html.php');
+
+// *** //
+$dictionary = new Dictionary($dbh);
+$dictionary->firstOrNew('slovnyk.ua', 'http://www.slovnyk.ua/?swrd=');
+$dictionaryId = (int) $dictionary->getProperty('id');
+
+$part_of_language = 'вигук';
+
+for ($j = 0; $j < 1;  $j++) {
+    echo '.';
+    $htmlObj = new Html($dbh);
+    $allHtml = $htmlObj->getPartOfLanguage('%' . $part_of_language . '%', 100, $j*100, 'LIKE');
+
+    foreach ($allHtml as $htmlArray) {
+        echo '+';
+        $html = new Html($dbh);
+        $html->getById(array_get($htmlArray, 'id'));
+
+        echo 'InitId(' . array_get($htmlArray, 'id') . ')';
+
+        // load extracted HTML=page
+        $word = cleanCyrillic($html->getProperty('word'));
+        $partOfLanguage = $html->getProperty('part_of_language');
+        $url = $html->getProperty('url');
+
+        if ($part_of_language !== trim($partOfLanguage)) {
+            $html->firstOrNewByPartOfLanguage($word, $part_of_language, $dictionaryId);
+            $html->updateProperty('url', PDO::PARAM_STR, $url);
+            $html->updateProperty('url_binary', PDO::PARAM_STR, $url);
+        }
+
+        $html->updateProperty('is_main_form', PDO::PARAM_BOOL, true);
+    }
+}
+
+echo 'END';
+
+
+
